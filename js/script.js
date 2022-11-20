4b4c68fd14205ec8fda0b17e511522633061256b
WelangaiEric <103843314+WelangaiEric@users.noreply.github.com>
2022-11-21 01:39:54 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,9 +18,7 @@ Submitbtn.addEventListener('click',function(e){
             cardName.style.borderColor='hsl(0, 100%, 66%)'
 
         }
-        if(cardNum.value==''){
-    
-        }
+        
     
         if(dateErr.value==''||dateErr2.value==''){
             document.querySelector('.cant1').style.display='initial'
@@ -35,7 +33,7 @@ Submitbtn.addEventListener('click',function(e){
         }
     }
     else{
-        if(cardName !=''&& cardNum!=''&&dateErr!=''&&dateErr2!=''&&cvcErr3!=''){
+        if(cardName.value !=''&& cardNum.value!=''&&dateErr.value!=''&&dateErr2.value!=''&&cvcErr3.value!=''){
             Errormsg.style.display='none' 
             subForm.style.display='none';
             confirm.style.display='flex';
@@ -53,5 +51,24 @@ Submitbtn.addEventListener('click',function(e){
 document.querySelector('.exit').addEventListener('click', function(e){
   alert('YEAH,THATS JUST IT😂😂!')
 });
+// dom elements
+cardName.addEventListener("keyup", function () {
+    document.querySelector('.cardNameDisplay').textContent = cardName.value;
+  });
+dateErr.addEventListener("keyup", function () {
+    document.querySelector('.cardMMDisplay').textContent = dateErr.value;
+  });
+  
+  dateErr2.addEventListener("keyup", function () {
+    document.querySelector('.cardYYDisplay').textContent = dateErr2.value;
+  });
+  cardNum.addEventListener("keyup", function () {
+    document.querySelector('.cardNumDisplay').textContent = cardNum.value;
+  });
+  cvcErr3.addEventListener("keyup", function () {
+    document.querySelector('.cvcDisplay').textContent = cvcErr3.value;
+  });
+ 
+
 
 
